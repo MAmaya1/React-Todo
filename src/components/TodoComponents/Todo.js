13b515d35d57aad0089f0093deeb2e1fd04576ad
Mario Amaya <thefozcat@gmail.com>
2019-03-21 15:45:38 -0700

    updated general styles

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Todo(props) {
     return (
         <div 
-            className={`${props.item.completed ? "completed" : ''}`}
+            className={`items ${props.item.completed ? "completed" : ''}`}
             onClick={() => props.toggleItem(props.item.id)}
         >
             <p>{props.task}</p>
